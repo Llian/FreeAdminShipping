9a47d0b7cb24d13dc75da827eaa16b48363375b8
Sander Mangel <sander@sandermangel.nl>
2017-01-26 12:10:30 +0100

    added signature to helper

--- app/code/community/Llian/FreeAdminShipping/Helper/Data.php
+++ app/code/community/Llian/FreeAdminShipping/Helper/Data.php
@@ -4,15 +4,15 @@
  *
  * Copyright (C) 2013  Llian
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the 
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program.  If not, 
+ * You should have received a copy of the GNU General Public License along with this program.  If not,
  * see <http://www.gnu.org/licenses/gpl.html>.
  *
  * @category  Llian
@@ -22,6 +22,13 @@
  * @license   http://www.gnu.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
  */
 
+/**
+ * Helper
+ *
+ * @category  Llian
+ * @package   Llian_FreeAdminShipping
+ * @author    Llian <info@llian.de>
+ */
 class Llian_FreeAdminShipping_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
